32a2467ce6d0bc1811f72d9832565610b9602d01
Meg Ducharme <ducharme.meg@gmail.com>
2018-04-07 20:24:58 -0500

    styling

--- scripts/DOMinteraction.js
+++ scripts/DOMinteraction.js
@@ -29,16 +29,13 @@ function printToDOM(student) {
     let event = (student.eventType === "ForkEvent") ? "Forked: " : "Last push: "
 
     stringToDOM += `
-        <div class="card center col">
-            <div class="card-body">
-                <h4>${student.name.name}</h4>
-                <p class="${student.color}">${event} ${student.diffDays}</p>
-                <a href="https://github.com/${student.repoURL}" target="_blank"><p style="color:black;">${student.repo}</p></a>
-                <p>${student.message}</p>
-                <a href="https://github.com/${student.githubHandle}" target="_blank">Student's Repo</a>
-            </div>
+        <div class="card">
+            <h4>${student.name.name}</h4>
+            <p class="${student.color}">${event} ${student.diffDays}</p>
+            <a href="https://github.com/${student.repoURL}" target="_blank"><p style="color:black;">${student.repo}</p></a>
+            <p>${student.message}</p>
+            <a href="https://github.com/${student.githubHandle}" target="_blank">Student's Repo</a>
         </div>`
-
 
     $("#output").html(stringToDOM)
 }
