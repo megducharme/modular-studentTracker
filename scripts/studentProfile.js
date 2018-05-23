031c75f547ebb697f9051707be05b65e4340d724
Meg Ducharme <ducharme.meg@gmail.com>
2018-05-22 21:25:58 -0500

    bring 22 back :(

--- scripts/studentProfile.js
+++ scripts/studentProfile.js
@@ -2,18 +2,22 @@ let gitHubInteraction = require ("./gitHub.js");
 
 let cohort = "";
 
-$("#classBtn").on("click", function (event) {
-    $("#jsPoints").hide()
+$(".class-buttons").each(index => {
+    $(this).on("click", function(){
+        $("#jsPoints").hide();
+        $("#footer").hide();
 
-    let jsonAddress = event.target.id
-    if(jsonAddress.startsWith("c__")){
-        $("#output").html("")
-        let cohort = `../students/c${jsonAddress.split("__")[1]}.json`
-        getStudentData(cohort).then(students => {
-            gitHubInteraction(students)
-        })
-    }
+        let jsonAddress = $(this).attr("id");
+        if(jsonAddress.startsWith("c__")){
+            $("#output").html("")
+            let cohort = `../students/c${jsonAddress.split("__")[1]}.json`
+            getStudentData(cohort).then(students => {
+                gitHubInteraction(students)
+            })
+        }
+    })
 })
+
 
 let getStudentData = cohort => {
     $(".loader-gif").hide()
